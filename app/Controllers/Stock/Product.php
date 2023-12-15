2f9fb40f4d3f2ca4cca6c6eb202a1d6d1e8725e2
nemesis-9 <nyxn96@gmail.com>
2023-12-15 17:27:15 +0530

    Stock Controllers Setup

---
+++ app/Controllers/Stock/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers\Stock;
+
+use App\Controllers\BaseController;
+
+class StockController extends BaseController
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function view($id)
+    {
+    }
+
+    public function update($id)
+    {
+    }
+
+    public function destroy($id)
+    {
+    }
+}
---
+++ app/Controllers/Stock/Stock.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Stock;
+
+use App\Controllers\BaseController;
+
+class Stock extends BaseController
+{
+    public function index()
+    {
+    }
+
+}
