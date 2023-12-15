b6e6df7ecf09062626eec475f40817fbfdc97430
nemesis-9 <nyxn96@gmail.com>
2023-12-15 17:01:12 +0530

    Stockkeeper Controllers Setup

---
+++ app/Controllers/Management/Stockkeeper/Dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers\Management\Stockkeeper;
+
+use App\Controllers\BaseController;
+
+class Dashboard extends BaseController
+{
+    public function index()
+    {
+    }
+}
