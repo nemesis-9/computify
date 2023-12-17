d7793b8b7e2765ed7b99bed05b45bd1c98edc8a9
nemesis-9 <nyxn96@gmail.com>
2023-12-17 07:21:49 +0530

    Update Home Controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,14 @@ class Home extends BaseController
     {
         return view('home');
     }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function register()
+    {
+        return view('register');
+    }
 }
