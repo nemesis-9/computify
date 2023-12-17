cf7b947f19f5067910e3d57ff6179a8ac504ded0
nemesis-9 <nyxn96@gmail.com>
2023-12-17 07:30:12 +0530

    Routes Update: Guest

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,19 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+
+/* ----- Main Routes ----- */
+$routes->view("about", "about");
+
+
+/* ----- Routes: Guest ----- */
+
+// Redirections
+$routes->addRedirect("guest", "/");
+$routes->addRedirect("guest/(:any)", "/$1");
+
+// Main Routings
 $routes->get('/', 'Home::index');
+$routes->get('login', 'Home::login');
+$routes->get('register', 'Home::register');
