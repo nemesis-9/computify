d013de7053565842b9f016ce941d030546876bc4
nemesis-9 <nyxn96@gmail.com>
2023-12-17 08:23:20 +0530

    Routes: index Functions

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,3 +21,81 @@ $routes->addRedirect("guest/(:any)", "/$1");
 $routes->get('/', 'Home::index');
 $routes->get('login', 'Home::login');
 $routes->get('register', 'Home::register');
+
+
+/* ----- Routes: Customer ----- */
+
+$routes->group('user', function ($routes) {
+
+    // Redirections
+    $routes->addRedirect('about', 'about');
+
+    // Main Routings
+    $routes->get('/', 'Home::index/user');
+
+});
+
+
+/* ----- Routes: Manager ----- */
+
+$routes->group('manager', function ($routes) {
+
+    // Redirections
+    // $routes->addRedirect('about', 'about');
+
+    // Main Routings
+    $routes->get('/', 'Management\Manager\Dashboard::index');
+
+});
+
+
+/* ----- Routes: Deliverer ----- */
+
+$routes->group('deliverer', function ($routes) {
+
+    // Redirections
+    // $routes->addRedirect('about', 'about');
+
+    // Main Routings
+    $routes->get('/', 'Management\Deliverer\Dashboard::index');
+
+});
+
+
+/* ----- Routes: Staff ----- */
+
+$routes->group('staff', function ($routes) {
+
+    // Redirections
+    // $routes->addRedirect('about', 'about');
+
+    // Main Routings
+    $routes->get('/', 'Management\Staff\Dashboard::index');
+
+});
+
+
+/* ----- Routes: Stockkeeper ----- */
+
+$routes->group('stockkeeper', function ($routes) {
+
+    // Redirections
+    // $routes->addRedirect('about', 'about');
+
+    // Main Routings
+    $routes->get('/', 'Management\Stockkeeper\Dashboard::index');
+
+});
+
+
+/* ----- Routes: Technician ----- */
+
+$routes->group('technician', function ($routes) {
+
+    // Redirections
+    // $routes->addRedirect('about', 'about');
+
+    // Main Routings
+    $routes->get('/', 'Management\Technician\Dashboard::index');
+
+});
